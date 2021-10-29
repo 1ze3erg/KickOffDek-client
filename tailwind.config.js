04bb3b902a0faf8b45cccba1d40752228d29f5e3
izeberg <skizeza666@gmail.com>
2021-10-30 00:53:06 +0700

    initialize KickOffDek-client

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        container: {
+            screens: {
+                sm: "100%",
+                md: "100%",
+                lg: "1024px",
+                xl: "1200px",
+            },
+        },
+        extend: {
+            spacing: {
+                72: "18rem",
+                84: "21rem",
+                96: "24rem",
+                144: "36rem",
+                150: "37.5rem",
+                192: "48rem",
+            },
+        },
+    },
+
+    variants: {
+        extend: {},
+    },
+    plugins: [require("@tailwindcss/line-clamp")],
+};
