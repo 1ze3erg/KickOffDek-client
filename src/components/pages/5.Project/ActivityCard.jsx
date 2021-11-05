4f32a72101e46b895e132b791b5d1382273f1cc5
izeberg <skizeza666@gmail.com>
2021-11-05 11:20:16 +0700

    use html parser

--- src/components/pages/5.Project/ActivityCard.jsx
+++ src/components/pages/5.Project/ActivityCard.jsx
@@ -1,22 +1,22 @@
-import React from "react";
+import parse from "html-react-parser";
 
 function ActivityCard({ title, message, updatedAt }) {
-  return (
-    <div className="flex justify-start flex-col border border-gray-500 rounded-lg w-10/12 mx-auto p-5 m-5">
-      <p>{updatedAt.slice(0, 10)}</p>
-      <h1 className="text-xl font-bold">{title}</h1>
-      <p>{message}</p>
-      <hr className="m-2" />
-      <div>
-        <button className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white">
-          Share
-        </button>
-        <button className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white">
-          Read more
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex justify-start flex-col border border-gray-500 rounded-lg w-10/12 mx-auto p-5 m-5">
+            <p>{updatedAt.slice(0, 10)}</p>
+            <h1 className="text-xl font-bold">{title}</h1>
+            <p>{parse(message)}</p>
+            <hr className="m-2" />
+            <div>
+                <button className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white">
+                    Share
+                </button>
+                <button className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white">
+                    Read more
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default ActivityCard;
--- src/components/pages/5.Project/ProjectCampaign.jsx
+++ src/components/pages/5.Project/ProjectCampaign.jsx
@@ -1,63 +1,43 @@
 import React, { useState, useEffect } from "react";
-import axios from "../../../config/axios";
 import { useParams } from "react-router-dom";
+import axios from "../../../config/axios";
+import parse from "html-react-parser";
 
 function ProjectCampaign() {
-  const [project, setProject] = useState([]);
-  const { projectId } = useParams();
+    const [project, setProject] = useState([]);
+    const { projectId } = useParams();
 
-  useEffect(() => {
-    try {
-      const fetchProject = async () => {
-        const res = await axios.get(`/projects/get-by-id/${projectId}`);
-        setProject(res.data);
-      };
-      fetchProject();
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
+    useEffect(() => {
+        try {
+            const fetchProject = async () => {
+                const res = await axios.get(`/projects/get-by-id/${projectId}`);
+                setProject(res.data);
+            };
+            fetchProject();
+        } catch (error) {
+            console.log(error);
+        }
+    }, []);
 
-  const { campaignImage, pitchVideo, campaignStory } = project;
-  console.log(project);
+    const { campaignImage, pitchVideo, campaignStory } = project;
+    console.log(project);
 
-  return (
-    <div className="grid grid-cols-4 w-10/12 mx-auto">
-      <div className="col-span-3 mr-4">
-        <h1 className="text-2xl font-bold my-2">About this project</h1>
-        <img
-          className="my-2 mx-auto"
-          src={campaignImage}
-          alt="KickOffDek"
-          width="300"
-          height="200"
-        />
-        <p className="my-2">
-          {campaignStory ? (
-            campaignStory
-          ) : (
-            <h1 className="font-bold text-lg my-10">
-              Project description is currently not available
-            </h1>
-          )}
-        </p>
-        <iframe
-          className="my-2 mx-auto"
-          src={pitchVideo}
-          width="700"
-          height="500"
-        ></iframe>
-        {/* <p className="my-2">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          Exercitationem, suscipit? Fugit maiores vitae in explicabo, veritatis
-        </p>
-        <p className="my-2">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          Exercitationem, suscipit? Fugit maiores vitae in explicabo, veritatis
-        </p> */}
-      </div>
-    </div>
-  );
+    return (
+        <div className="grid grid-cols-4 w-10/12 mx-auto">
+            <div className="col-span-3 mr-4">
+                <h1 className="text-2xl font-bold my-2">About this project</h1>
+                <img className="my-2 mx-auto" src={campaignImage} alt="KickOffDek" width="300" height="200" />
+                <p className="my-2">
+                    {campaignStory ? (
+                        parse(campaignStory)
+                    ) : (
+                        <h1 className="font-bold text-lg my-10">Project description is currently not available</h1>
+                    )}
+                </p>
+                <iframe className="my-2 mx-auto" src={pitchVideo} width="700" height="500"></iframe>
+            </div>
+        </div>
+    );
 }
 
 export default ProjectCampaign;
